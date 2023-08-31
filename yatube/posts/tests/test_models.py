01d400767739fdd6c8ffca5f5e4ae1fb5c63c946
Vladimir Maksimov <111915960+v-mcsimoff@users.noreply.github.com>
2023-08-31 18:55:35 +0200

    Update test_models.py

--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -12,17 +12,17 @@ class PostModelTest(TestCase):
         super().setUpClass()
         cls.user = User.objects.create_user(username='auth')
         cls.group = Group.objects.create(
-            title='Тестовая группа',
-            slug='Тестовый слаг',
-            description='Тестовое описание',
+            title='Test group',
+            slug='Test slug',
+            description='Test description',
         )
         cls.post = Post.objects.create(
             author=cls.user,
-            text='Тестовый новый пост',
+            text='Test new post',
         )
 
     def test_models_have_correct_object_names(self):
-        """Проверяем, что у моделей корректно работает __str__."""
+        """Check that __str__ works correctly for models."""
         post = PostModelTest.post
         expected_object_name = post.text
         self.assertEqual(expected_object_name, str(post))
